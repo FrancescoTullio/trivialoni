82ccadd3a84c5e6c2673501a4e2246657ef29f2f
FrancescoTullio <francescotullio1992@gmail.com>
2025-10-10 12:37:03 +0200

    feat: enhance category selection with descriptions and update routing for quiz
    con enrico liveshare

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -3,17 +3,19 @@ import { Outlet } from "react-router-dom";
 import AppHeader from "../components/AppHeader";
 
 function AppLayout() {
-    return (
-        <>
-            <div>
-                <AppHeader />
-                <main>
-                    <Outlet />
-                </main>
-                <AppFooter />
-            </div>
-        </>
-    )
-};
+  return (
+    <>
+      <div>
+        <AppHeader />
+        <main>
+          <div className="container">
+            <Outlet />
+          </div>
+        </main>
+        <AppFooter />
+      </div>
+    </>
+  );
+}
 
 export default AppLayout;
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,16 +1,32 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import GlobalContext from "../context/GlobalContext";
 function Category() {
-
-
+  const { GlobalProviderValue } = useContext(GlobalContext);
+  const { categoryArray } = GlobalProviderValue;
 
   return (
     <>
-    <h1>scegli la tua categoria </h1>
-    {/* <Link to="/category/9" > 
-      General Knowledge
-    </Link> */}
-    
+      <h1 className="text-center">scegli la tua categoria </h1>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {categoryArray.map((e) => (
+        <div className="card">
+          <div className="card-header">{e.name}</div>
+          <div className="card-body">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">
+              {e.description}
+            </p>
+            <Link to={`/quiz/${e.id}`} className="btn btn-primary">
+              vai alk quiz
+            </Link>
+          </div>
+        </div>
+      ))}
+      </div>
+      
     </>
-  )
+  );
 }
 
-export default  Category
+export default Category;
